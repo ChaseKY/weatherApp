a37b742bedb713d17adffc063a8341d5ecade240
Chase Young <chasekyoung@gmail.com>
2022-02-08 17:54:09 -0500

    weather forecast functions properly now

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+var container = document.querySelector("#container");
 var input = document.querySelector("#userInput");
 var button = document.querySelector("#searchBtn");
 var cityname = document.querySelector("#cityName");
@@ -6,14 +7,22 @@ var current = document.querySelector("#currentWeather");
 var humid = document.querySelector("#humidity");
 var wind = document.querySelector("#windSpeed");
 
-button.addEventListener("click", function(){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=fcd594fc4b7accf846134105a10b76ac&units=imperial`).then(function(response) {
-    return response.json()
-}).then(function (weather){
-    console.log(weather);
-    displayWeather(weather)
-    
-})
+button.addEventListener("click", function () {
+    container.innerHTML = ""
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=fcd594fc4b7accf846134105a10b76ac&units=imperial`).then(function (response) {
+        return response.json()
+    }).then(function (weather) {
+        console.log(weather);
+        displayWeather(weather)
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&exclude=current,minutely,hourly,alerts&appid=fcd594fc4b7accf846134105a10b76ac&units=imperial`).then(function (response) {
+            return response.json()
+        }).then(function (forecast) {
+            console.log(forecast)
+            fiveDayForecast(forecast)
+
+        })
+    })
 })
 
 function displayWeather(weather) {
@@ -24,10 +33,29 @@ function displayWeather(weather) {
     wind.textContent = weather.wind.speed;
 }
 
-function fiveDayForecast() {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=fcd594fc4b7accf846134105a10b76ac&units=imperial`).then(function(response) {
-    return response.json()
-}).then(function (weather) {
-    
-})
+function fiveDayForecast(forecast) {
+    debugger
+    for (let index = 0; index < 5; index++) {
+        var card = document.createElement("div");
+        card.classList.add("card");
+        var cardHeader = document.createElement("div");
+        cardHeader.classList.add("card-header");
+        var unorderList = document.createElement("ul");
+        unorderList.className = "list-group list-group-flush";
+        var listTemp = document.createElement("li");
+        listTemp.classList.add("list-group-item");
+        var listWind = document.createElement("li");
+        listWind.classList.add("list-group-item");
+        var listHumid = document.createElement("li");
+        listHumid.classList.add("list-group-item");
+
+        container.appendChild(card);
+        card.append(cardHeader, unorderList);
+        unorderList.append(listTemp, listWind, listHumid);
+        
+        cardHeader.textContent = moment().add(1, 'days').format('L');
+        listTemp.textContent = "Temperature: " + forecast.daily[index].temp.day;
+        listWind.textContent = "Wind speed: " + forecast.daily[index].wind_speed;
+        listHumid.textContent = "Humidity: " + forecast.daily[index].humidity;
+    }
 }
